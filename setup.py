0a74dcf89ab934b375ea1abf248acbf2d0183d64
cwd-k2 <kaito.augmented7th@gmail.com>
2021-01-09 12:36:46 +0900

    なんかインストールできたりできなかったりする

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
             'proof_leader=proof_leader:main'
         ],
     },
+    packages = ['proof_leader', 'proof_leader.notifier', 'proof_leader.internal'],
     author='cwd-k2',
     url='https://github.com/cwd-k2/ProofLeader',
     python_requires='>=3.2',
