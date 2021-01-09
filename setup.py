9bb5d087488dcc1f8a25f557c80d6dc27aa7420a
cwd-k2 <kaito.augmented7th@gmail.com>
2021-01-09 09:48:13 +0900

    下準備

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='ProofLeader_cwd-k2',
+    version='3.0.0',
     entry_points={
         'console_scripts': [
             'proof_leader=proof_leader:main'
         ],
     },
     author='cwd-k2',
+    url='https://github.com/cwd-k2/ProofLeader',
     python_requires='>=3.2',
 )
